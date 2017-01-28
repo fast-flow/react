5043172668e790144a9000e038b647a7c91cb72a
Nimo <nimo.jser@gmail.com>
2017-01-28 13:38:39 +0800

    npm.taobao.org & remove console.log

--- template/__test__/basic.test.js
+++ template/__test__/basic.test.js
@@ -2,17 +2,7 @@ import { Component } from "react"
 import {{ componentname name }} from "../index"
 import { shallow, mount, render } from "enzyme"
 
-class App extends Component {
-    constructor (props) {
-        super(props)
-    }
-    render () {
-        return (
-            <{{ componentname name }} className="myclass" />
-        )
-    }
-}
 it('className', function () {
-    const app = mount(<App/>)
+    const app = mount(<{{ componentname name }} className="myclass" />)
     expect(app.find('.myclass').length).toEqual(1)
 })
--- template/config/webpack.ghversion.js
+++ template/config/webpack.ghversion.js
@@ -2,5 +2,4 @@ var path = require('path')
 var config = require('./webpack.gh')
 var iPackage = require('../package.json')
 config.output.path = path.join(__dirname, '../output', iPackage.version)
-console.log(config)
 module.exports = config
