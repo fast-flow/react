531b253b25db7abc300330310ff5f135a857c68b
Nimo <nimo.jser@gmail.com>
2017-02-22 12:14:28 +0800

    safari platform => Windows 7

--- template/dev-config.js
+++ template/dev-config.js
@@ -40,7 +40,7 @@ module.exports = {
         sl_mac_safari: {
             base: 'SauceLabs',
             browserName: 'safari',
-            platform: 'OS X 10.10'
+            platform: 'Windows 7'
         },
         /*
         // mobile
